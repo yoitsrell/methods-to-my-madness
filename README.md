08ff699b1e13414e5e2838c421096c3d6f6b1641
Colin Jaffe <balloonasaurus@gmail.com>
2020-02-26 16:26:28 -0500

    Removes extraneous code.

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Tests should make these exercises self-explanatory. Read them to see what your output should be given different inputs!
 
 Run `npm install` to install the Jest testing library and `npm test` to make the testing magic happen.
+
+
+
--- main.js
+++ main.js
@@ -2,9 +2,79 @@
  * YOUR CODE HERE *
  ******************/
 
+function slice(str, start = 0, end = str.length) {
+  let sliced = '';
 
+  for (let i = start; i < end; i++) {
+    sliced += str[i];
+  }
 
+  return sliced;
+}
 
+function repeat(str, repetitions) {
+  let count = 1;
+  let repeated = '';
+
+  while (count <= repetitions) {
+    repeated += str;
+
+    count++;
+  }
+
+  return repeated;
+}
+
+function startsWith(str, substr) {
+  // for (let i = 0; i < substr.length; i++) {
+  // if (str[i] !== substr[i]) {
+      // return false;
+    // }
+  // }
+
+  // return true;
+
+  return slice(str, 0, substr.length) === substr;
+}
+
+function endsWith(str, substr) {
+  return slice(str, str.length - substr.length) === substr;
+}
+
+function includes(arr, item) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function join(arr, separator = '') {
+  let joined = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    joined += separator + arr[i];
+  }
+
+  return joined;
+}
+
+function split(str, separator = '') {
+  const splitsville = [];
+  let start = 0; 
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === separator) {
+      splitsville.push(slice(str, start, i));
+      start = i + 1;
+    }
+  }
+
+  splitsville.push(slice(str, start));
+
+  return splitsville;
+}
 
 
 /*********************************
